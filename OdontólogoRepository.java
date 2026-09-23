4f5dec60a21c4147418651a1df7607f32ef8fe83
dedominicisiris204-sys <232719367+dedominicisiris204-sys@users.noreply.github.com>
2026-09-23 12:15:34 -0300

    Crear OdontólogoRepository.java

---
+++ OdontólogoRepository.java
@@ -0,0 +1,6 @@
+package com.proyectohoussay.backendodonto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OdontologoRepository extends JpaRepository<Odontologo, Long> {
+}
